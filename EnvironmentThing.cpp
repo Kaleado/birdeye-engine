88e477b1a1fb660483ea952b55ce09536d8e448e
Kaleado <kaleadoskope@hotmail.com>
2018-03-23 15:41:32 +1100

    Made EnvironmentThings not bounce off each other, edited level background to show a path leading off the map, and added some trees and stuff.

--- EnvironmentThing.cpp
+++ EnvironmentThing.cpp
@@ -6,7 +6,8 @@ void EnvironmentThing::handleCollision(std::weak_ptr<Thing> other){
     return;
   }
   std::shared_ptr<Bullet> asBullet = std::dynamic_pointer_cast<Bullet>(otherPtr);
-  if(!_canWalkThrough && !asBullet){
+  std::shared_ptr<EnvironmentThing> asEnvironmentThing = std::dynamic_pointer_cast<EnvironmentThing>(otherPtr);
+  if(!_canWalkThrough && !asBullet && !asEnvironmentThing){
     otherPtr->preventOverlapping(shared_from_this(), 6.5);
   }
   if(!_canShootThrough){

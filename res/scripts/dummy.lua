4bb0674a20fa74ac228328e5311f35ab3a547303
Kaleado <kaleadoskope@hotmail.com>
2018-10-09 03:35:00 +1100

    yeet

--- res/scripts/dummy.lua
+++ res/scripts/dummy.lua
@@ -6,8 +6,49 @@
 -- To change this template use File | Settings | File Templates.
 --
 
-function dummy (arg1, arg2)
-    print("this is a dummy function!")
-    return arg1 == arg2
+myPos = nil
+targetVector = nil
+
+function pressPrimaryFire()
+    print("Press primary fire from "..myPos.x.." "..myPos.y.." to "..targetVector.x.." "..targetVector.y)
+    return
 end
 
+function releasePrimaryFire()
+    print("Release primary fire.")
+    return
+end
+
+function pressSecondaryFire()
+    print("Press secondary fire.")
+    return
+end
+
+function releaseSecondaryFire()
+    print("Release secondary fire.")
+    return
+end
+
+function tick()
+    return
+end
+
+function onEquip()
+    print("Equip.")
+    return
+end
+
+function onUnequip()
+    print("Unequip.")
+    return
+end
+
+function onStow()
+    print("Stow.")
+    return
+end
+
+function onUnstow()
+    print("Unstowed.")
+    return
+end
--- src/ScriptedWeapon.cpp
+++ src/ScriptedWeapon.cpp
@@ -14,23 +14,40 @@ void ScriptedWeapon::_initScript() {
 
 //!Handles when the player presses/releases the primary/secondary fire buttons.
 void ScriptedWeapon::pressPrimaryFire(sf::Vector2f target){
-    luabridge::LuaRef dummy = luabridge::getGlobal(_l, "dummy");
-    dummy(1,2);
+    float x = player->getWorldPosition().x;
+    float y = player->getWorldPosition().y;
+    float tx = target.x;
+    float ty = target.y;
+
+    luabridge::getGlobalNamespace(_l)
+        .beginNamespace("myPos")
+            .addVariable("x", &x, false)
+            .addVariable("y", &y, false)
+            .endNamespace();
+
+    luabridge::getGlobalNamespace(_l)
+        .beginNamespace("myPos")
+            .addVariable("x", &x, false)
+            .addVariable("y", &y, false)
+            .endNamespace();
+
+    luabridge::LuaRef dummy = luabridge::getGlobal(_l, "pressPrimaryFire");
+    dummy();
 }
 
 void ScriptedWeapon::pressSecondaryFire(sf::Vector2f target){
-    luabridge::LuaRef dummy = luabridge::getGlobal(_l, "dummy");
-    dummy(1,1);
+    luabridge::LuaRef dummy = luabridge::getGlobal(_l, "pressSecondaryFire");
+    dummy();
 }
 
 void ScriptedWeapon::releasePrimaryFire(sf::Vector2f target){
-    luabridge::LuaRef dummy = luabridge::getGlobal(_l, "dummy");
-    dummy(1,2);
+    luabridge::LuaRef dummy = luabridge::getGlobal(_l, "releasePrimaryFire");
+    dummy();
 }
 
 void ScriptedWeapon::releaseSecondaryFire(sf::Vector2f target){
-    luabridge::LuaRef dummy = luabridge::getGlobal(_l, "dummy");
-    dummy(1,1);
+    luabridge::LuaRef dummy = luabridge::getGlobal(_l, "releaseSecondaryFire");
+    dummy();
 }
 
 //!Executed every frame - implement weapon logic in this function.
@@ -39,24 +56,24 @@ void ScriptedWeapon::tick(){
 
 //!Executed when the weapon is (un)equipped in the player's weapon bar.
 void ScriptedWeapon::onEquip(){
-    luabridge::LuaRef dummy = luabridge::getGlobal(_l, "dummy");
-    dummy(1,2);
+    luabridge::LuaRef dummy = luabridge::getGlobal(_l, "onEquip");
+    dummy();
 }
 
 void ScriptedWeapon::onUnequip(){
-    luabridge::LuaRef dummy = luabridge::getGlobal(_l, "dummy");
-    dummy(1,2);
+    luabridge::LuaRef dummy = luabridge::getGlobal(_l, "onUnequip");
+    dummy();
 }
 
 //!Executed when the player switches to and away from the weapon.
 void ScriptedWeapon::onStow(){
-    luabridge::LuaRef dummy = luabridge::getGlobal(_l, "dummy");
-    dummy(1,2);
+    luabridge::LuaRef dummy = luabridge::getGlobal(_l, "onStow");
+    dummy();
 }
 
 void ScriptedWeapon::onUnstow(){
-    luabridge::LuaRef dummy = luabridge::getGlobal(_l, "dummy");
-    dummy(1,2);
+    luabridge::LuaRef dummy = luabridge::getGlobal(_l, "onUnstow");
+    dummy();
 }
 
 ScriptedWeapon::ScriptedWeapon(std::string script) {
